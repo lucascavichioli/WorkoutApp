7170041d82f3204efd002ad3245dd3a1cb1a8d85
Lucas Cavichioli <lucascavica@gmail.com>
2022-12-07 20:31:59 -0300

    Add project files.

---
+++ WorkoutApp/Controllers/TrainingPlanController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WorkoutApp.Models;
+
+namespace WorkoutApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TrainingPlanController : ControllerBase
+    {
+        private static List<TrainingPlan> plans = new List<TrainingPlan>();
+
+        [HttpPost]
+        public void AddTrainingPlan([FromBody] TrainingPlan plan) 
+        {
+            plans.Add(plan);
+            Console.WriteLine(plan.Author);
+        }
+
+        [HttpGet]
+        public IEnumerable<TrainingPlan> GetTrainingPlan() 
+        {
+            return plans;
+        }
+
+    }
+}
---
+++ WorkoutApp/Models/TrainingPlan.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WorkoutApp.Models
+{
+    public class TrainingPlan
+    {
+        [Key]
+        [Required]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public int Author { get; set; } //reference
+
+        public string? ImageLink { get; set; }
+
+        public short Length { get; set; } //quantas semanas
+
+        public short PerWeek { get; set; } //quantos dias por semana
+
+        public short PerSession { get; set; } //quantos minutos por sessão de treino
+
+        public int Category { get; set; } // Corrida / Cardio / HIIT reference
+
+        public string? LocationDifficulty { get; set; } // reference
+
+        public string? Comments { get; set; } 
+        public int Likes { get; set; }
+
+    }
+}
